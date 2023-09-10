978ca458132f633f3f6b61ae4a3ff8e33b2e938c
d1y <chenhonzhou@gmail.com>
2023-09-10 18:45:44 +0800

    add index/ping route

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module d1y.io/study
 
 go 1.21.1
+
+require (
+	github.com/Code-Hex/Neo-cowsay/v2 v2.0.4 // indirect
+	github.com/Code-Hex/go-wordwrap v1.0.0 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	_ "embed"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+
+	cowsay "github.com/Code-Hex/Neo-cowsay/v2"
 )
+
+//go:embed template/index.html
+var indexHtmlData string
 
 type ResultVO struct {
 	Success bool   `json:"success"`
@@ -38,16 +45,31 @@ func (ig *IBlog) index(w http.ResponseWriter, r *http.Request) {
 	w.Write(data)
 }
 
+func (ig *IBlog) indexHtml(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "html")
+	w.Write([]byte(indexHtmlData))
+}
+
+func (ig *IBlog) pingPong(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("I'm live"))
+}
+
 func (ig *IBlog) Register() {
 	http.HandleFunc("/", ig.index)
+	http.HandleFunc("/index", ig.indexHtml)
+	http.HandleFunc("/ping", ig.pingPong)
 }
 
 func (ig *IBlog) Run() error {
 	ig.Register()
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", ig.Port), nil); err != nil {
-		return err
-	}
-	return nil
+	go func() {
+		if err := http.ListenAndServe(fmt.Sprintf(":%d", ig.Port), nil); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	say, _ := cowsay.Say(fmt.Sprintf("server is running http://localhost:%d", ig.Port))
+	fmt.Println(say)
+	select {}
 }
 
 func main() {
